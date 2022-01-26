52e2b45867bb12e0254aa3db708f8254fa6f6a7f
Tahsin <tahsintubepc@gmail.com>
2022-01-26 22:55:52 +0400

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// const nav_set = document.querySelector('.nav_set')
+const toggler_icon = document.querySelector('.navbar-toggler-icon')
+const top_header2 = document.getElementById('top_header2')
+// // const header = document.querySelector(".header_section")
+let mq1 = window.matchMedia('(max-width: 991px)')
+
+
+
+
